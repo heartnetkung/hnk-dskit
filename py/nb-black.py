852bb75eee5201d419b9d5c04216698621826122
contact.englishmate@gmail.com <heartnetkung@gmail.com>
2019-01-01 10:06:51 +0000

    add nb-black

---
+++ py/nb-black.py
@@ -0,0 +1,5 @@
+# # Installation
+get_ipython().system(u' pip install nb_black')
+# # Usage
+# Copy this command over to your notebook and run every cell. In the process, your cell outputs might be deleted. If that's the case, restart your runtime and run every cell again without this command.
+get_ipython().magic(u'load_ext lab_black')
